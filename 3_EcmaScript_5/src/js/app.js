283f9b52f27d0672bac6ea8a8996ae2b4386282b
Mac <Mac@MacBook-Pro-Mac-2.local>
2019-01-18 23:54:06 +0300

    Решена проблема с ошибокой второго и третьего места

--- 3_EcmaScript_5/src/js/app.js
+++ 3_EcmaScript_5/src/js/app.js
@@ -4,16 +4,20 @@ const numberOfPlace = { //Здесь хранятся данные о призо
 	theThird: 0,
 };
 
-for (const playersPointsAr of [74989, 74990, 74990, 62000, 58480, 61800]) { //набранные баллы за очки в игре
-	if (playersPointsAr > numberOfPlace.theFirst){ //находим 1-е место
-		numberOfPlace.theFirst = playersPointsAr;
-	} else if (playersPointsAr > numberOfPlace.theSecond && playersPointsAr != numberOfPlace.theFirst){ //находим 2-е место
-		numberOfPlace.theSecond = playersPointsAr;
-	} else if (playersPointsAr > numberOfPlace.theThird && playersPointsAr != numberOfPlace.theSecond
-	&& playersPointsAr != numberOfPlace.theFirst){ //находим 3-е место
-		numberOfPlace.theThird = playersPointsAr;
+for (var i = 0; i < 3; i++) { //прогоняется три раза, что бы не пропускать нужное число
+	for (const playersPointsAr of [74989, 74990, 74990, 62000, 58480, 61800]) { //набранные баллы за очки в игре
+
+		if (playersPointsAr > numberOfPlace.theFirst){ //находим 1-е место
+			numberOfPlace.theFirst = playersPointsAr;
+		} else if (playersPointsAr > numberOfPlace.theSecond && playersPointsAr != numberOfPlace.theFirst){ //находим 2-е место
+			numberOfPlace.theSecond = playersPointsAr;
+		} else if (playersPointsAr > numberOfPlace.theThird && playersPointsAr != numberOfPlace.theSecond
+		&& playersPointsAr != numberOfPlace.theFirst){ //находим 3-е место
+			numberOfPlace.theThird = playersPointsAr;
+		}
 	}
 }
+	
 
 let averagePoints = Math.round((numberOfPlace.theFirst + numberOfPlace.theSecond + numberOfPlace.theThird) / 3);
 
